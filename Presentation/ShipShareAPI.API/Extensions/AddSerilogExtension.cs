799d55c68929dc25d538cf8a00591f607ecfd416
Salamzade Islam Emil <Salam_ar93@itstep.edu.az>
2024-02-26 20:15:18 +0400

    Global exception handling and Logging added

---
+++ Presentation/ShipShareAPI.API/Extensions/AddSerilogExtension.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Cors.Infrastructure;
+using Serilog;
+using Serilog.Core;
+using Serilog.Sinks.MSSqlServer;
+using Serilog.Sinks.MSSqlServer.Sinks.MSSqlServer.Options;
+using System.Collections.ObjectModel;
+
+namespace ShipShareAPI.API.Extensions
+{
+    public static class AddSerilogExtension
+    {
+
+        public static void AddSerilog(this WebApplicationBuilder builder)
+        {
+            var sinkOptions = new MSSqlServerSinkOptions()
+            {
+                TableName = "logs",
+                AutoCreateSqlTable = true,
+            };
+
+            //var colOptions = new ColumnOptions()
+            //{
+            //    AdditionalColumns = new Collection<SqlColumn>()
+            //            {
+            //                new SqlColumn()
+            //                {
+            //                    ColumnName = "user_name",
+            //                    DataType = System.Data.SqlDbType.NVarChar,
+            //                },
+            //            }
+            //};
+
+            Logger log = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.File("logs/log.txt")
+                .WriteTo.MSSqlServer(
+                builder.Configuration.GetConnectionString("ShipShareConStr"),
+                sinkOptions: sinkOptions
+                //columnOptions: colOptions
+                )
+                .Enrich.FromLogContext()
+                .CreateLogger();
+            builder.Host.UseSerilog(log);
+        }
+    }
+}
---
+++ Presentation/ShipShareAPI.API/Extensions/ConfigureExceptionHandlerExtension.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using System.Net;
+using System.Net.Mime;
+using System.Text.Json;
+
+namespace ShipShareAPI.API.Extensions
+{
+    public static class ConfigureExceptionHandlerExtension
+    {
+        public static void ConfigureExceptionHandler<T>(this WebApplication application,ILogger<T> logger)
+        {
+            application.UseExceptionHandler(builder =>
+            {
+                builder.Run(async (context) =>
+                {
+                    context.Response.StatusCode = (int)(HttpStatusCode.InternalServerError);
+                    context.Response.ContentType = MediaTypeNames.Application.Json;
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (contextFeature is not null)
+                    {
+                        logger.LogError(contextFeature.Error.Message);
+                        await context.Response.WriteAsync(JsonSerializer.Serialize(new
+                        {
+                            StatusCode = (int)HttpStatusCode.InternalServerError,
+                            Message = contextFeature.Error.Message,
+                            Title = "Unexpected error!"
+                        }));
+                    };
+                });
+            });
+        }
+    }
+}
